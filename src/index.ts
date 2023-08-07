89562cf3bf5819653b38366741064952c52155b0
Robbie <robbiethewadley@gmail.com>
2023-08-07 15:25:01 -0600

    restore behavior

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,21 @@ import { sendMessage } from "./sendMessage";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// const loop = async () => {
-//   console.log("getting updated data");
-//   const pulls = await getPullData();
+const loop = async () => {
+  console.log("getting updated data");
+  const pulls = await getPullData();
 
-//   const devBlocks = makeDevBlocks(pulls);
-//   await sendMessage(env.DEV_CHANNEL_ID, devBlocks, false);
+  const devBlocks = makeDevBlocks(pulls);
+  await sendMessage(env.DEV_CHANNEL_ID, devBlocks, false);
 
-//   const compactBlocks = await makeCompactBlocks(pulls);
-//   await sendMessage(env.COMPACT_CHANNEL_ID, compactBlocks, false);
+  const compactBlocks = await makeCompactBlocks(pulls);
+  await sendMessage(env.COMPACT_CHANNEL_ID, compactBlocks, false);
 
-//   await sleep(1000 * 60);
+  await sleep(1000 * 60);
 
-//   exec("git fetch && git pull");
+  exec("git fetch && git pull");
 
-//   loop();
-// };
+  loop();
+};
 
-// loop();
-
-
-// print the version of node and exit
-console.log(process.version);
-process.exit(0);
+loop();
