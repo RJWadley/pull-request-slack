03ac7eeeb94ca6bf525f21c0ce68fcbde36fb9a3
Robbie <robbiethewadley@gmail.com>
2024-05-07 12:42:57 -0600

    link tweak

--- src/makeDevBlocks.ts
+++ src/makeDevBlocks.ts
@@ -73,7 +73,7 @@ export const makeDevBlocks = (pulls: MappedPull[]) => {
                 : pull.checkState === "failing"
                 ? ":pull_fail:"
                 : ":pull_wait:"
-            } ${pull.draft ? "*[  DRAFT  ]*\t" : ""}[${pull.number}](${pull.link})\t${
+            } ${pull.draft ? "*[  DRAFT  ]*\t" : ""}*<${pull.link}|${pull.number}>*\t${
               pull.title
             }`,
           },
