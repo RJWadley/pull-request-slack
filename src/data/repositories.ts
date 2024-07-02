1baf0f734338d79e52cadf58b72e04f1112a9bd8
Robbie <robbiethewadley@gmail.com>
2024-07-02 15:34:48 -0600

    add recurrency

--- src/data/repositories.ts
+++ src/data/repositories.ts
@@ -1,13 +1,14 @@
 export default [
-  "reformcollective/reform-gatsby-starter",
-  "reformcollective/library",
-  // "reformcollective/Runway_V2",
-  // "reformcollective/legwork",
-  // "reformcollective/reform_3.0",
-  "reformcollective/FlowFi",
-  "aerodome-usa/aerodome.com-v2",
-  // "reformcollective/legwork-marketing",
-  // "boost-insurance/boost",
-  "reformcollective/impilo",
-  "reformcollective/thoughtly"
-]
+	"reformcollective/reform-gatsby-starter",
+	"reformcollective/library",
+	// "reformcollective/Runway_V2",
+	// "reformcollective/legwork",
+	// "reformcollective/reform_3.0",
+	"reformcollective/FlowFi",
+	"aerodome-usa/aerodome.com-v2",
+	// "reformcollective/legwork-marketing",
+	// "boost-insurance/boost",
+	"reformcollective/impilo",
+	"reformcollective/thoughtly",
+	"reformcollective/recurrency",
+];
